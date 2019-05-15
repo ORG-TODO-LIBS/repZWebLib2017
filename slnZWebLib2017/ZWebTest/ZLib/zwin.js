6a7cebc2b39dbaf6ebb5b5d6043fb7d204036b44
zhangjr <ruilovechu@live.com>
2019-05-15 13:51:50 +0800

    添加函数 getcontextmenupos 未测试

--- slnZWebLib2017/ZWebTest/ZLib/zwin.js
+++ slnZWebLib2017/ZWebTest/ZLib/zwin.js
@@ -1,6 +1,21 @@
 ﻿window.zwin = {
 
     /**
+     * 基于指定的上下文菜单宽度及高度，及点击右键传入的事件参数 event。计算出合适的上下文菜单位置。
+     * @param {any} contextwidth 上下文菜单宽度
+     * @param {any} contextheight 上下文菜单高度
+     * @param {any} ev 点击右键事件参数
+     */
+    getcontextmenupos(contextwidth, contextheight, ev) {
+        var x = ev.clientX > document.body.clientWidth - contextwidth ? document.body.clientWidth - contextwidth : ev.clientX;
+        var y = ev.clientY > document.body.clientHeight - contextheight ? document.body.clientHeight - contextheight : ev.clientY;
+        return {
+            x: x,
+            y: y
+        };
+    },
+
+    /**
      * 全屏
      * @param {any} 无参数
      */
